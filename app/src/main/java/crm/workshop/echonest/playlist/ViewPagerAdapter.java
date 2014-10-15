1288dda78ef59e514c4ea841007ebfad0d83ba3f
Louis Beltramo <louis@beltramo.me>
2014-10-15 16:13:15 +0200

    Dynamic fragment for ViewPager

--- app/src/main/java/crm/workshop/echonest/playlist/ViewPagerAdapter.java
+++ app/src/main/java/crm/workshop/echonest/playlist/ViewPagerAdapter.java
@@ -2,19 +2,28 @@ package crm.workshop.echonest.playlist;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.HashMap;
 
 /**
  * Created by lbeltramo on 15/10/2014.
  */
-public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    HashMap<Integer, String> fragmentMap;
+
     public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
+        fragmentMap = new HashMap<Integer, String>();
+        fragmentMap.put(0, "Alec Empire");
+        fragmentMap.put(1, "U2");
+        fragmentMap.put(2, "Primal Fear");
     }
 
     @Override
     public Fragment getItem(int i) {
-        return PlayListFragment.newInstance(10, "Alec Empire");
+        return PlayListFragment.newInstance(10, fragmentMap.get(i));
     }
 
     @Override
